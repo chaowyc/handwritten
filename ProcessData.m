6c28d2007cb518c05995af48180e6b296af0d6ca
chaowyc <1454744690@qq.com>
2016-01-06 23:09:43 +0800

    add the label for train data and test data

--- ProcessData.m
+++ ProcessData.m
@@ -1,4 +1,4 @@
-function [trainData, testData] = ProcessData(dataSet, trainDataSize, testDataSize)
+function [trainData, trainDataLabel, testData, testDataLabel] = ProcessData(dataSet, trainDataSize, testDataSize)
 %函数功能描述：
 %对输入的数据集dataSet，根据trainDataSize和testDataSize划分为相应大小的训练数据集和测试数据集,并返回
 %TODO：实现交叉验证，函数实现参考scikit-learn的交叉验证模块实现：http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.train_test_split.html
@@ -11,8 +11,10 @@ function [trainData, testData] = ProcessData(dataSet, trainDataSize, testDataSiz
 %   TODO：trainDataSize和testDataSize的范围进行限制
 %output:
 %   trainData:根据trainDataSize对dataSet划分之后的训练数据集
+%   trainDataLabel:根据trainDataSize对dataSet划分之后的训练数据集所对应的标签
 %   testData:根据testDataSize对testSet划分之后的测试数据集
-%(c) 王永超, 2016
+%   testDataLabel:根据testDataSize对testSet划分之后的测试数据集所对应的标签
+%(c) 王永超 哈尔滨工业大学计算机科学与技术, 2016
 if nargin == 1
     trainDataSize = 100;
     testDataSize = 50;
@@ -45,26 +47,39 @@ end
 %% 分割数据集，　每一类前trainDataSize条为训练数据　后testDataSize条数据为测试数据集
 trainData = zeros(c * trainDataSize, 256);
 testData = zeros(c * testDataSize, 256);
+trainDataLabel = zeros(c * trainDataSize, 1);
+testDataLabel = zeros(c * testDataSize, 1);
 
-trainData(1: trainDataSize, :) = img(1 : trainDataSize, :);
+trainData(1 : trainDataSize, :) = img(1 : trainDataSize, :);
+trainDataLabel(1 : trainDataSize, 1) = dec_T(1 : trainDataSize, 1);
 testData(1 : testDataSize, :) = img(trainDataSize + 1 : trainDataSize + testDataSize, :);
+testDataLabel(1 : testDataSize, :) = dec_T(trainDataSize + 1 : trainDataSize + testDataSize, :);
+
 for i = 2 : c
     i
     tmp = (i - 1) * trainDataSize + 1; 
     indexTmp = index(i - 1);
     trainData(tmp : i * trainDataSize, :) = img(indexTmp + 1 : indexTmp + trainDataSize, :);
+    trainDataLabel(tmp : i * trainDataSize, :) = dec_T(indexTmp + 1 : indexTmp + trainDataSize, :);
     tmp = (i - 1) * testDataSize + 1;
     testData(tmp : i * testDataSize, :) = img(indexTmp + trainDataSize + 1 : indexTmp + trainDataSize + testDataSize, :);
+    testDataLabel(tmp : i * testDataSize, :) = dec_T(indexTmp + trainDataSize + 1 : indexTmp + trainDataSize + testDataSize, :);
 end
 %% 通过isequal(),测试数据集的分割正确与否。
 isequal(trainData(1:trainDataSize, :), img(1:trainDataSize, :))
 isequal(testData(1:testDataSize, :), img(trainDataSize + 1 : trainDataSize + testDataSize, :))
+
+isequal(trainDataLabel(1:trainDataSize, :), dec_T(1:trainDataSize, :))
+isequal(testDataLabel(1:testDataSize, :), dec_T(trainDataSize + 1 : trainDataSize + testDataSize, :))
+
 for i = 2 : c
     tmp = (i - 1) * trainDataSize + 1; 
     indexTmp = index(i - 1);
     isequal(trainData(tmp : i * trainDataSize, :), img(indexTmp + 1 : indexTmp + trainDataSize, :))
+    isequal(trainDataLabel(tmp : i * trainDataSize, :), dec_T(indexTmp + 1 : indexTmp + trainDataSize, :))
     tmp = (i - 1) * testDataSize + 1;
     isequal(testData(tmp : i * testDataSize, :), img(indexTmp + trainDataSize + 1 : indexTmp + trainDataSize + testDataSize, :))
+    isequal(testDataLabel(tmp : i * testDataSize, :), dec_T(indexTmp + trainDataSize + 1 : indexTmp + trainDataSize + testDataSize, :))
 end
 
 
--- main.m
+++ main.m
@@ -11,8 +11,9 @@ load data\semeion.data
 %   4. 数据集分割，每一个类别选取trainDataSize的数据组成训练数据集，选取testDataSize的数据组成测试数据集
 %   5. DataSet 为需要处理的数据集 optional 是可选的trainDataSize 和
 %   testDataSize，如果没有显示指定，trainDataSize=100 testDataSize=50
+%   函数最终返回处理之后的训练数据集和对应的类标签 以及 测试数据及和对应的标签
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[trainData, testData] = ProcessData(semeion);
+[trainData, trainDataLabel, testData, testDataLabel] = ProcessData(semeion);
 
 
 
