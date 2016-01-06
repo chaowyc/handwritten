571ef89537aadcb9e882aa8c7e10abd30b1f22d5
chaowyc <1454744690@qq.com>
2016-01-06 22:45:06 +0800

    finished the process part of the project

---
+++ ProcessData.m
@@ -0,0 +1,70 @@
+function [trainData, testData] = ProcessData(dataSet, trainDataSize, testDataSize)
+%函数功能描述：
+%对输入的数据集dataSet，根据trainDataSize和testDataSize划分为相应大小的训练数据集和测试数据集,并返回
+%TODO：实现交叉验证，函数实现参考scikit-learn的交叉验证模块实现：http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.train_test_split.html
+%input:
+%   dataSet 需要处理的数据集
+%   参数trainDataSize 和 testDataSize是可选的，缺省为
+%   trainDataSize = 100
+%   testDataSize = 50
+%   范围也是有限制的
+%   TODO：trainDataSize和testDataSize的范围进行限制
+%output:
+%   trainData:根据trainDataSize对dataSet划分之后的训练数据集
+%   testData:根据testDataSize对testSet划分之后的测试数据集
+%(c) 王永超, 2016
+if nargin == 1
+    trainDataSize = 100;
+    testDataSize = 50;
+end
+
+%% 加载数据， 数据来源http://archive.ics.uci.edu/ml/
+[m, n] = size(dataSet);
+
+%% 取出样本的像素点阵 前256维 取出样本的类标签 后10维
+img = dataSet(:, 1:256);
+T = dataSet(:, 257:266);
+
+%% 将标签转化为十进制数并由dec_T存储
+dec_T = zeros(m, 1);
+for i = 1 : m
+    id = find(T(i, :) == 1);
+    dec_T(i, 1) = id;
+end
+
+%% 去除重复的标签，并计算每一个类别的样本数目，此部分仅为了解数据集的组成，为划分数据集为训练数据集　测试数据集的规模做参考
+label = unique(dec_T);
+c = size(label, 1);
+index = zeros(c, 1);
+index(1, 1) = size(find(dec_T == label(1, 1)), 1);
+for i = 2 : c
+    id = find(dec_T == label(i, 1));
+    index(i, 1) = index(i - 1, 1) + size(id, 1);
+end
+
+%% 分割数据集，　每一类前trainDataSize条为训练数据　后testDataSize条数据为测试数据集
+trainData = zeros(c * trainDataSize, 256);
+testData = zeros(c * testDataSize, 256);
+
+trainData(1: trainDataSize, :) = img(1 : trainDataSize, :);
+testData(1 : testDataSize, :) = img(trainDataSize + 1 : trainDataSize + testDataSize, :);
+for i = 2 : c
+    i
+    tmp = (i - 1) * trainDataSize + 1; 
+    indexTmp = index(i - 1);
+    trainData(tmp : i * trainDataSize, :) = img(indexTmp + 1 : indexTmp + trainDataSize, :);
+    tmp = (i - 1) * testDataSize + 1;
+    testData(tmp : i * testDataSize, :) = img(indexTmp + trainDataSize + 1 : indexTmp + trainDataSize + testDataSize, :);
+end
+%% 通过isequal(),测试数据集的分割正确与否。
+isequal(trainData(1:trainDataSize, :), img(1:trainDataSize, :))
+isequal(testData(1:testDataSize, :), img(trainDataSize + 1 : trainDataSize + testDataSize, :))
+for i = 2 : c
+    tmp = (i - 1) * trainDataSize + 1; 
+    indexTmp = index(i - 1);
+    isequal(trainData(tmp : i * trainDataSize, :), img(indexTmp + 1 : indexTmp + trainDataSize, :))
+    tmp = (i - 1) * testDataSize + 1;
+    isequal(testData(tmp : i * testDataSize, :), img(indexTmp + trainDataSize + 1 : indexTmp + trainDataSize + testDataSize, :))
+end
+
+
---
+++ main.m
@@ -0,0 +1,18 @@
+%% 加载数据， 数据来源http://archive.ics.uci.edu/ml/
+load data\semeion.data
+
+%% 函数ProcessData(DataSet, optional)对数据集进行预处理 包括：
+%   函数用法 
+%   ProcessData(DataSet)
+%   ProcessData(DataSet, trainDataSize, testDataSize)
+%   1. 提取出样本像素点 样本的标签
+%   2. 将样本的标签转为十进制数，如1000000000 -> 1 0100000000 -> 2等等
+%   3. 标签去重
+%   4. 数据集分割，每一个类别选取trainDataSize的数据组成训练数据集，选取testDataSize的数据组成测试数据集
+%   5. DataSet 为需要处理的数据集 optional 是可选的trainDataSize 和
+%   testDataSize，如果没有显示指定，trainDataSize=100 testDataSize=50
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[trainData, testData] = ProcessData(semeion);
+
+
+
